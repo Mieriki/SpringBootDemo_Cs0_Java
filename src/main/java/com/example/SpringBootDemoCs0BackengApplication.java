02ce09e79126ed8952de23e85dbcd4de02f6f9ae
Mieriki <chicken1233@163.com>
2024-03-26 10:37:21 +0800

    项目初始化

---
+++ src/main/java/com/example/SpringBootDemoCs0BackengApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoCs0BackengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoCs0BackengApplication.class, args);
+    }
+
+}
